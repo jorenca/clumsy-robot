5d7bf839313df9f84735c295425fcf91ee740f14
jorenca <mailnamefrustration@gmail.com>
2019-06-09 00:03:00 +0300

    Add movement speed slider

--- bot-control/src/components/Movement.js
+++ bot-control/src/components/Movement.js
@@ -1,64 +1,79 @@
 import React from 'react';
+import './Movement.scss';
 
 export default class Telemetry extends React.Component {
   state = {
-    speed: 60
+    speed: '60'
   };
 
-  doMove = (left, right) => {
+  move = (left, right) => {
     const { speed } = this.state;
     fetch(`/move/${left}/${speed}/${right}/${speed}`);
   };
 
+  setSpeed = e => this.setState({ speed: e.target.value });
+
   render() {
-    const move = this.doMove;
+    const { speed } = this.state;
+    const { move, setSpeed } = this;
     return (
-      <div>
+      <div className="movement-container">
         Movement
 
-        <table>
-          <tbody>
-            <tr>
-              <td />
-              <td />
-              <td><Control onClick={() => move(1, 1)}/></td>
-              <td />
-              <td />
-            </tr>
-            <tr>
-              <td />
-              <td />
-              <td><Control onClick={() => move(0.2, 0.2)}/></td>
-              <td />
-              <td />
-            </tr>
-            <tr>
-              <td><Control onClick={() => move(1, -1)} /></td>
-              <td><Control onClick={() => move(0.2, -0.2)} /></td>
-              <td style={{ backgroundColor: 'gray' }} />
-              <td><Control onClick={() => move(-0.2, 0.2)}/></td>
-              <td><Control onClick={() => move(-1, 1)}/></td>
-            </tr>
-            <tr>
-              <td />
-              <td />
-              <td><Control onClick={() => move(-0.2, -0.2)}/></td>
-              <td />
-              <td />
-            </tr>
-            <tr>
-              <td />
-              <td />
-              <td><Control onClick={() => move(-1, -1)}/></td>
-              <td />
-              <td />
-            </tr>
-          </tbody>
-        </table>
+        <SpeedSlider speed={speed} setSpeed={setSpeed} />
+        <Keypad move={move} />
       </div>
     );
   }
 }
+
+const SpeedSlider = ({ speed, setSpeed }) =>
+<div className="speed-slider">
+  <input type="range" min="10" max="99" value={speed} onChange={setSpeed} />
+</div>;
+
+const Keypad = ({ move }) =>
+<div className="keypad">
+  <table>
+    <tbody>
+      <tr>
+        <td />
+        <td />
+        <td><Control onClick={() => move(1, 1)}/></td>
+        <td />
+        <td />
+      </tr>
+      <tr>
+        <td />
+        <td />
+        <td><Control onClick={() => move(0.2, 0.2)}/></td>
+        <td />
+        <td />
+      </tr>
+      <tr>
+        <td><Control onClick={() => move(-1, 1)} /></td>
+        <td><Control onClick={() => move(-0.2, 0.2)} /></td>
+        <td style={{ backgroundColor: 'gray' }} />
+        <td><Control onClick={() => move(0.2, -0.2)}/></td>
+        <td><Control onClick={() => move(1, -1)}/></td>
+      </tr>
+      <tr>
+        <td />
+        <td />
+        <td><Control onClick={() => move(-0.2, -0.2)}/></td>
+        <td />
+        <td />
+      </tr>
+      <tr>
+        <td />
+        <td />
+        <td><Control onClick={() => move(-1, -1)}/></td>
+        <td />
+        <td />
+      </tr>
+    </tbody>
+  </table>
+</div>;
 
 const Control = ({ onClick }) =>
 <button onClick={onClick} style={{ width: '50px', height: '50px' }}>
