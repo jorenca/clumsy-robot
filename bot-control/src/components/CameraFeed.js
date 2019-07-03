3c0ae54e3f5f85c3709708d57740aa6367a3c299
jorenca <mailnamefrustration@gmail.com>
2019-07-03 23:17:19 +0300

    Add proximity gauge slider.

--- bot-control/src/components/CameraFeed.js
+++ bot-control/src/components/CameraFeed.js
@@ -3,7 +3,20 @@ import './CameraFeed.scss';
 
 const CameraFeed = ({ proximity }) =>
 <div className="feed-container">
-  {proximity}
+  <ProximityGauge proximity={Number.parseInt(proximity || '255')} />
 </div>;
+
+const MAX_SENSOR_READING = 200;
+const ProximityGauge = ({ proximity }) => {
+  const valueInBounds = Math.min(proximity, MAX_SENSOR_READING);
+  const text = proximity > MAX_SENSOR_READING ? '>50cm or <4cm' : proximity + 'cm';
+
+  return (
+    <div className="proximity-gauge-container">
+      <input type="range" min="10" max="200" value={proximity} orient="vertical" name="proximity-gauge-slider" />
+      <div>{text}</div>
+    </div>
+  );
+};
 
 export default CameraFeed;
