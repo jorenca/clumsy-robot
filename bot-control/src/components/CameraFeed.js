0fa502ac899780cfe97f0f665bb6322a9b93db8b
jorenca <mailnamefrustration@gmail.com>
2019-07-04 20:39:22 +0300

    Cleanup proximity gauge

--- bot-control/src/components/CameraFeed.js
+++ bot-control/src/components/CameraFeed.js
@@ -6,18 +6,16 @@ const CameraFeed = ({ proximity }) =>
   <ProximityGauge proximity={Number.parseInt(proximity || '255')} />
 </div>;
 
-const MAX_SENSOR_DIST = 47; //cm
 const MAX_SENSOR_READING = 203;
-const MIN_SENSOR_DIST = 4; //cm
-const MIN_SENSOR_READING = 10;
+const MIN_SENSOR_READING = 18;
 const SENSOR_READING_RANGE = MAX_SENSOR_READING - MIN_SENSOR_READING;
-const SENSOR_DIST_RANGE = MAX_SENSOR_DIST - MIN_SENSOR_DIST;
 const distReadingMapper = x => 0.237 * x - 0.39;
 const ProximityGauge = ({ proximity }) => {
   const valueInBounds = Math.max(Math.min(proximity, MAX_SENSOR_READING), MIN_SENSOR_READING);
+  const distCm = distReadingMapper(valueInBounds);
   const percentage = (valueInBounds - MIN_SENSOR_READING) / SENSOR_READING_RANGE;
 
-  const text = proximity > MAX_SENSOR_READING ? '>50cm or <4cm' : distReadingMapper(valueInBounds) + 'cm';
+  const text = proximity > MAX_SENSOR_READING ? '>50cm or <4cm' : distCm + 'cm';
 
   return (
     <div className="proximity-gauge-container">
