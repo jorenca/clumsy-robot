ed56c1e6643a27f7d02f8a2a1fcc26daca0aee3d
jorenca <mailnamefrustration@gmail.com>
2019-07-04 20:07:11 +0300

    Fix camera distance readout

--- bot-control/src/components/CameraFeed.js
+++ bot-control/src/components/CameraFeed.js
@@ -6,17 +6,18 @@ const CameraFeed = ({ proximity }) =>
   <ProximityGauge proximity={Number.parseInt(proximity || '255')} />
 </div>;
 
-const MAX_SENSOR_DIST = 50; //cm
-const MAX_SENSOR_READING = 200;
+const MAX_SENSOR_DIST = 47; //cm
+const MAX_SENSOR_READING = 203;
 const MIN_SENSOR_DIST = 4; //cm
 const MIN_SENSOR_READING = 10;
 const SENSOR_READING_RANGE = MAX_SENSOR_READING - MIN_SENSOR_READING;
 const SENSOR_DIST_RANGE = MAX_SENSOR_DIST - MIN_SENSOR_DIST;
+const distReadingMapper = x => 0.238 * x - 1.39;
 const ProximityGauge = ({ proximity }) => {
   const valueInBounds = Math.max(Math.min(proximity, MAX_SENSOR_READING), MIN_SENSOR_READING);
   const percentage = (valueInBounds - MIN_SENSOR_READING) / SENSOR_READING_RANGE;
 
-  const text = proximity > MAX_SENSOR_READING ? '>50cm or <4cm' : (MIN_SENSOR_READING + percentage * SENSOR_DIST_RANGE) + 'cm';
+  const text = proximity > MAX_SENSOR_READING ? '>50cm or <4cm' : distReadingMapper(valueInBounds) + 'cm';
 
   return (
     <div className="proximity-gauge-container">
