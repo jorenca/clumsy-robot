62a111d84729a5a0768dc9821b86ecc4669a3415
jorenca <mailnamefrustration@gmail.com>
2019-07-10 19:41:46 +0300

    Add resent last command to direct motor board command panel.

--- bot-control/src/components/DirectCommand.js
+++ bot-control/src/components/DirectCommand.js
@@ -3,45 +3,67 @@ import './DirectCommand.scss';
 
 class DirectCommand extends React.Component {
   state = {
-    visible: false
+    visible: false,
+    lastCmd: null
   };
 
   sendCommand = cmd => {
-    this.setState({ visible: false });
+    this.setState({ lastCmd: cmd });
     fetch(`/motor_cmd/${cmd}`);
   };
 
   toggleInputVisibility = () => this.setState({ visible: !this.state.visible });
 
   render() {
-    const { visible } = this.state;
+    const { visible, lastCmd } = this.state;
     return (
       <div className="direct-command-container">
         <button onClick={this.toggleInputVisibility}>Send direct command</button>
-        {visible && <CommandInput onSend={this.sendCommand} />}
+        {visible && <CommandInputModal onSend={this.sendCommand} last={lastCmd} />}
       </div>
     );
   }
 }
 
-const CommandInput = ({ onSend }) =>
-<div className="command-input">
-  <form onSubmit={e => {
-    e.preventDefault();
-    onSend(e.target.command.value);
-  }}>
-    <textarea rows="10" name="command" />
-    <pre>
-      M leftRotations leftRPM rightRotations rightRPM<br />
-      D[L|R] timeMs stepFrequency direction<br/>
-      CSTOP<br />
-      <br />
-      <br />
-      CMD1;<br />
-      M_CMD1:M_CMD2:M_CMD3:...;
-    </pre>
-    <button type="submit">Send</button>
-  </form>
-</div>;
+class CommandInputModal extends React.Component {
+  state = {
+    cmd: ''
+  };
+
+  render() {
+    const { cmd } = this.state;
+    const { onSend, last } = this.props;
+    return (
+      <div className="command-input">
+        <textarea
+          rows="10"
+          value={cmd}
+          onChange={e => this.setState({ cmd: e.target.value })}
+        />
+        <button onClick={() => onSend(cmd)}>Send</button>
+        
+        <pre>
+          M leftRotations leftRPM rightRotations rightRPM<br />
+          D[L|R] timeMs stepFrequency direction<br/>
+          CSTOP<br />
+          <br />
+          <br />
+          CMD1;<br />
+          M_CMD1:M_CMD2:M_CMD3:...;
+        </pre>
+
+        {last && <div className="last-sent-container">
+          <pre>
+            Last sent:<br />
+            {last}
+          </pre>
+          <button onClick={() => onSend(last) }>
+            Resend
+          </button>
+        </div>}
+      </div>
+    );
+  }
+}
 
 export default DirectCommand;
