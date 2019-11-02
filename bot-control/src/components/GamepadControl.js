8ebed93651ef6df846df7c87f67a978946f52f4f
jorenca <mailnamefrustration@gmail.com>
2019-11-02 17:00:21 +0200

    Add proper gamepad constrol support

--- bot-control/src/components/GamepadControl.js
+++ bot-control/src/components/GamepadControl.js
@@ -1,54 +1,78 @@
-import React from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import _ from 'lodash';
 import Gamepad from 'react-gamepad';
 
-// left and right revs (float), speed up to 99 rpm
-const moveCallUnthrottled = (left, right, speed) => fetch(`/move/${left}/${speed}/${right}/${speed}`);
-const moveCall = _.throttle(moveCallUnthrottled, 500);
 const revsInHalfSec = (speed) => speed /* rpm */ / (60 * 2);
 
-const Move = {
-  up: (speed) => moveCall(revsInHalfSec(speed), revsInHalfSec(speed), speed),
-  down: (speed) => moveCall(-revsInHalfSec(speed), -revsInHalfSec(speed), speed),
-  left: (speed) => moveCall(revsInHalfSec(speed), 0, speed),
-  right: (speed) => moveCall(0, revsInHalfSec(speed), speed)
-};
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
 
-const AxisData = {
-  x: 0,
-  y: 0
-};
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
 
-const AxisUpdate = {
-  LeftStickY: (value) => AxisData.y = value,
-  LeftStickX: (value) => AxisData.x = value
-};
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
 
-const applyMovementLoop = () => {
-  if (AxisData.x === 0 && AxisData.y === 0) {
-    return;
-  }
-
-  if (Math.abs(AxisData.y) > Math.abs(AxisData.x)) {
-    AxisData.y > 0 ? Move.up(AxisData.y) : Move.down(Math.abs(AxisData.y));
-  } else {
-    AxisData.x < 0 ? Move.left(Math.abs(AxisData.x)) : Move.right(AxisData.x);
-  }
-};
-
-setInterval(applyMovementLoop, 450); // some tolerance
-
-const MIN_SPEED = 50;
+const MIN_SPEED = 20;
 const MAX_SPEED = 99;
-const onAxisChange = (axis, value) => {
-  console.log(axis, ' / ', value);
-  let requestedSpeed = value * MAX_SPEED;
-  if (Math.abs(requestedSpeed) < MIN_SPEED) {
-    requestedSpeed = 0;
-  }
+export default ({ moveUp, moveDown, moveLeft, moveRight, setHasGamepad }) => {
+  const [xAxisValue, setXAxis] = useState(0);
+  const [yAxisValue, setYAxis] = useState(0);
 
-  const applyAxisUpdate = AxisUpdate[axis] || _.noop;
-  applyAxisUpdate(requestedSpeed);
-};
+  const onAxisChange = (axis, value) => {
+    let requestedSpeed = value * MAX_SPEED;
+    if (Math.abs(requestedSpeed) < MIN_SPEED) {
+      requestedSpeed = 0;
+    }
 
-export default () => <Gamepad onAxisChange={onAxisChange} ><p /></Gamepad>;
+    const applyAxisUpdate = {
+      LeftStickY: setYAxis,
+      LeftStickX: setXAxis,
+      RightStickY: setYAxis,
+      RightStickX: setXAxis,
+    }[axis] || _.noop;
+    applyAxisUpdate(requestedSpeed);
+  };
+
+  useInterval(() => {
+    if (xAxisValue === 0 && yAxisValue === 0) {
+      return;
+    }
+
+    if (Math.abs(yAxisValue) > Math.abs(xAxisValue)) { // move ahead / back
+      const speed = yAxisValue;
+      if (speed > 0) {
+        moveUp(revsInHalfSec(speed), speed);
+      } else {
+        moveDown(revsInHalfSec(Math.abs(speed)), Math.abs(speed));
+      }
+    } else { // turn left or right
+      const speed = xAxisValue / 2;
+      if (speed < 0) {
+        moveLeft(revsInHalfSec(Math.abs(speed)), Math.abs(speed));
+      } else {
+        moveRight(revsInHalfSec(speed), speed);
+      }
+    }
+  }, 450);
+
+  return (
+    <Gamepad
+      onAxisChange={onAxisChange}
+      onConnect={() => setHasGamepad(true)}
+      onDisconnect={() => setHasGamepad(false)}>
+      <p />
+    </Gamepad>
+  );
+}
--- bot-control/src/components/Movement.js
+++ bot-control/src/components/Movement.js
@@ -1,34 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import GamepadControl from './GamepadControl.js';
 import './Movement.scss';
+import gamepadIcon from './gamepad_icon.png';
 
-export default class Movement extends React.Component {
-  state = {
-    speed: '60'
+export default function Movement() {
+  const [speed, setSpeed] = useState('60');
+
+  const move = (left, right, overrideSpeed) => {
+    const speedToUse = overrideSpeed || speed;
+    fetch(`/move/${left}/${speedToUse}/${right}/${speedToUse}`);
   };
+  const cstop = () => fetch('/cstop');
 
-  move = (left, right) => {
-    const { speed } = this.state;
-    fetch(`/move/${left}/${speed}/${right}/${speed}`);
-  };
-
-  setSpeed = e => this.setState({ speed: e.target.value });
-
-  cstop = () => fetch('/cstop');
-
-  render() {
-    const { speed } = this.state;
-    const { move, setSpeed, cstop } = this;
-    return (
-      <div className="movement-container">
-        Movement
-
-        <GamepadControl />
-        <SpeedSlider speed={speed} setSpeed={setSpeed} />
-        <Keypad move={move} cstop={cstop} />
-      </div>
-    );
-  }
+  return (
+    <div className="movement-container">
+      Movement
+      <SpeedSlider speed={speed} setSpeed={(e) => setSpeed(e.target.value)} />
+      <Keypad move={move} cstop={cstop} />
+    </div>
+  );
 }
 
 const SpeedSlider = ({ speed, setSpeed }) =>
@@ -36,54 +26,101 @@ const SpeedSlider = ({ speed, setSpeed }) =>
   <input type="range" min="10" max="99" value={speed} onChange={setSpeed} />
 </div>;
 
-const Keypad = ({ move, cstop }) =>
-<div className="keypad">
-  <table>
-    <tbody>
-      <tr>
-        <td />
-        <td />
-        <td><Control onClick={() => move(1, 1)}/></td>
-        <td />
-        <td />
-      </tr>
-      <tr>
-        <td />
-        <td />
-        <td><Control onClick={() => move(0.2, 0.2)}/></td>
-        <td />
-        <td />
-      </tr>
-      <tr>
-        <td><Control onClick={() => move(-1, 1)} /></td>
-        <td><Control onClick={() => move(-0.2, 0.2)} /></td>
-        <td style={{ backgroundColor: 'gray' }} />
-        <td><Control onClick={() => move(0.2, -0.2)}/></td>
-        <td><Control onClick={() => move(1, -1)}/></td>
-      </tr>
-      <tr>
-        <td />
-        <td />
-        <td><Control onClick={() => move(-0.2, -0.2)}/></td>
-        <td />
-        <td />
-      </tr>
-      <tr>
-        <td />
-        <td />
-        <td><Control onClick={() => move(-1, -1)}/></td>
-        <td />
-        <td>
-          <Control onClick={() => cstop()} style={{ backgroundColor: 'red' }}>
-            STOP
-          </Control>
-        </td>
-      </tr>
-    </tbody>
-  </table>
-</div>;
+const Keypad = ({ move, cstop }) => {
+  const [upActive, setUpActive] = useState(false);
+  const markUp = () => { setUpActive(true); setTimeout(setUpActive, 100, false); };
+  const moveUp = (dist, overrideSpeed) => { markUp(); move(dist, dist, overrideSpeed); };
 
-const Control = ({ onClick, children, style }) =>
-<button onClick={onClick} style={{ width: '50px', height: '50px', ...style }}>
+  const [downActive, setDownActive] = useState(false);
+  const markDown = () => { setDownActive(true); setTimeout(setDownActive, 100, false); };
+  const moveDown = (dist, overrideSpeed) => { markDown(); move(-dist, -dist, overrideSpeed); };
+
+  const [leftActive, setLeftActive] = useState(false);
+  const markLeft = () => { setLeftActive(true); setTimeout(setLeftActive, 100, false); };
+  const moveLeft = (dist, overrideSpeed) => { markLeft(); move(-dist, dist, overrideSpeed); };
+
+  const [rightActive, setRightActive] = useState(false);
+  const markRight = () => { setRightActive(true); setTimeout(setRightActive, 100, false); };
+  const moveRight = (dist, overrideSpeed) => { markRight(); move(dist, -dist, overrideSpeed); };
+
+  const [hasGamepad, setHasGamepad] = useState(false);
+
+  return (
+    <div className="keypad">
+      <GamepadControl
+        moveUp={moveUp}
+        moveDown={moveDown}
+        moveLeft={moveLeft}
+        moveRight={moveRight}
+        setHasGamepad={setHasGamepad}
+        />
+
+      <table>
+        <tbody>
+          <tr>
+            <td />
+            <td />
+            <td>
+              <Control onClick={() => moveUp(1)} isActive={upActive}>🡅🡅</Control>
+            </td>
+            <td />
+            <td>
+              <img
+                src={gamepadIcon}
+                alt="Gamepad support"
+                style={{
+                  width: '45px',
+                  height: '45px',
+                  backgroundColor: hasGamepad ? '#2cd027' : 'transparent'
+                }} />
+              </td>
+          </tr>
+          <tr>
+            <td />
+            <td />
+            <td><Control onClick={() => moveUp(0.2)}>🡅</Control></td>
+            <td />
+            <td />
+          </tr>
+          <tr>
+            <td><Control onClick={() => moveLeft(1)} isActive={leftActive}>&lt;&lt;</Control></td>
+            <td><Control onClick={() => moveLeft(0.2)}>&lt;</Control></td>
+            <td style={{ backgroundColor: 'gray' }} />
+            <td><Control onClick={() => moveRight(0.2)}>&gt;</Control></td>
+            <td><Control onClick={() => moveRight(1)} isActive={rightActive}>&gt;&gt;</Control></td>
+          </tr>
+          <tr>
+            <td />
+            <td />
+            <td><Control onClick={() => moveDown(0.2)}>🡇</Control></td>
+            <td />
+            <td />
+          </tr>
+          <tr>
+            <td />
+            <td />
+            <td><Control onClick={() => moveDown(1)} isActive={downActive}>🡇🡇</Control></td>
+            <td />
+            <td>
+              <Control onClick={() => cstop()} style={{ backgroundColor: 'red' }}>
+                STOP
+              </Control>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+const Control = ({ onClick, children, style, isActive }) =>
+<button
+  onClick={onClick}
+  style={{
+    width: '50px',
+    height: '50px',
+    backgroundColor: isActive ? '#2cd027' : '#e5e5e5',
+    ...style
+  }}>
   {children || 'X'}
 </button>
