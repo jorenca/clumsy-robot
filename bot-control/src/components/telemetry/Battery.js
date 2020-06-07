5d1c825c4b2ed35544cf47cd2e49f828e77e9d93
jorenca <mailnamefrustration@gmail.com>
2020-06-07 11:08:36 +0300

    More UI improvements for small screens

--- bot-control/src/components/telemetry/Battery.js
+++ bot-control/src/components/telemetry/Battery.js
@@ -13,26 +13,31 @@ const Battery = ({ battery = {} }) => {
 
   return (
     <div className="battery-indicator">
-      <table width="100%">
+      <table>
         <tbody>
-          <tr><td>Battery voltage</td><td>{_.floor(battery.batteryV, 1).toFixed(1)} V</td></tr>
-          <tr><td>Bus voltage</td><td>{_.floor(battery.busV, 1).toFixed(1)} V</td></tr>
-          <tr><td>Current</td><td>{_.ceil(battery.current, 2).toFixed(2)} A</td></tr>
-          <tr><td>Power draw</td><td>{_.ceil(battery.current * battery.batteryV, 1).toFixed(1)} W</td></tr>
+          <tr>
+            <td>Battery voltage</td><td>{_.floor(battery.batteryV, 1).toFixed(1)}V</td>
+            <td rowspan="4" width="110px">
+              <div>
+                <div className="perc-value">{_.round(percent)}%</div>
+
+                <div className="battery">
+                  <div id="battery-level" className={classNames({
+                    high: percent > 50,
+                    medium: percent <=50 && percent > 20,
+                    low: percent <= 20
+                  })} style={{ width: percent }}></div>
+                </div>
+              </div>
+            </td>
+          </tr>
+          <tr><td>Bus voltage</td><td>{_.floor(battery.busV, 1).toFixed(1)}V</td></tr>
+          <tr><td>Current</td><td>{_.ceil(battery.current, 2).toFixed(2)}A</td></tr>
+          <tr><td>Power draw</td><td>{_.ceil(battery.current * battery.batteryV, 1).toFixed(1)}W</td></tr>
+          <tr>
+          </tr>
         </tbody>
       </table>
-
-      <div>
-      <div className="perc-value">{_.round(percent)}%</div>
-
-        <div className="battery">
-          <div id="battery-level" className={classNames({
-            high: percent > 50,
-            medium: percent <=50 && percent > 20,
-            low: percent <= 20
-          })} style={{ width: percent }}></div>
-        </div>
-      </div>
     </div>
   );
 };
