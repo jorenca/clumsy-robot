f47596e8300279c0a07929c3d34670c5113b204c
jorenca <mailnamefrustration@gmail.com>
2020-12-23 16:27:25 +0200

    Update ROS bot model

--- ros/src/clumsybot_init/launch/rviz2.launch.py
+++ ros/src/clumsybot_init/launch/rviz2.launch.py
@@ -2,20 +2,48 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
+    return LaunchDescription([
+        get_rviz_node(),
+        get_state_publisher_node(),
+    ])
+
+
+def get_rviz_node():
     rviz_config_dir = os.path.join(
         get_package_share_directory('clumsybot_description'),
         'rviz',
         'model.rviz')
 
-    return LaunchDescription([
-        Node(
-            package='rviz2',
-            node_executable='rviz2',
-            node_name='rviz2',
-            arguments=['-d', rviz_config_dir],
-            output='screen'),
-    ])
+    return Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        arguments=['-d', rviz_config_dir],
+        output='screen')
+
+
+def get_state_publisher_node():
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+    urdf_file_name = os.path.join(
+        get_package_share_directory('clumsybot_description'),
+        'urdf',
+        'clumsybot.urdf')
+
+    print("URDF robot description file :" + urdf_file_name)
+    urdf_contents = open(urdf_file_name).read()
+
+    return Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{
+                'use_sim_time': use_sim_time,
+                'robot_description': urdf_contents
+            }])
