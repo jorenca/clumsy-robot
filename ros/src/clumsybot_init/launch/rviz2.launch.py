5db1e606d0dd4eba66b6551b1725e99042751740
jorenca <mailnamefrustration@gmail.com>
2020-12-23 22:31:34 +0200

    Import ROS simulation starting and map files

--- ros/src/clumsybot_init/launch/rviz2.launch.py
+++ ros/src/clumsybot_init/launch/rviz2.launch.py
@@ -2,8 +2,9 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
 from launch.substitutions import LaunchConfiguration, Command
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 import xacro
 
@@ -12,10 +13,12 @@ os.environ['LC_NUMERIC'] = "en_US.UTF-8" # Fix for URDF not showing correctly in
 
 
 def generate_launch_description():
-    return LaunchDescription([
-        get_state_publisher_gui_node(),
-        get_rviz_node(),
-        get_state_publisher_node(),
+    return LaunchDescription(
+        start_simulation() +
+        [
+            get_state_publisher_gui_node(),
+            get_rviz_node(),
+            get_state_publisher_node(),
     ])
 
 
@@ -61,3 +64,28 @@ def get_state_publisher_node():
                 'use_sim_time': use_sim_time,
                 'robot_description': urdf_contents
             }])
+
+
+def start_simulation():
+    use_sim_time = LaunchConfiguration('use_sim_time', default='True')
+    world_file_name = 'turtlebot3_houses/waffle.model'
+    world = os.path.join(get_package_share_directory('clumsybot_init'), 'worlds', world_file_name)
+    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+
+    print('Using %s as a world file' % world)
+    return [
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')
+            ),
+            launch_arguments={'world': world}.items(),
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py')
+            ),
+        ),
+        ExecuteProcess(
+            cmd=['ros2', 'param', 'set', '/gazebo', 'use_sim_time', use_sim_time],
+            output='screen'),
+    ]
--- ros/src/clumsybot_init/setup.py
+++ ros/src/clumsybot_init/setup.py
@@ -1,17 +1,26 @@
 from setuptools import setup
+import os
 
 package_name = 'clumsybot_init'
+
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            val = ('share/' + package_name + '/' + path,  [path + '/' + filename])
+            paths.append(val)
+    return paths
+
 
 setup(
     name=package_name,
     version='0.1.0',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name, ['launch/rviz2.launch.py'])
-    ],
+        ('share/' + package_name, ['launch/rviz2.launch.py']),
+    ] + package_files('worlds'),
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='georgi',
