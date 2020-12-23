9305100f50021c645ef366a8e88e98c03492f293
jorenca <mailnamefrustration@gmail.com>
2020-12-23 14:02:59 +0200

    Add initial basic ROS package structure

---
+++ ros/src/clumsybot_init/clumsybot_init/run_sim.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from clumsybot_init.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ ros/src/clumsybot_init/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+package_name = 'clumsybot_init'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name, ['launch/rviz2.launch.py'])
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='georgi',
+    maintainer_email='mailnamefrustration@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'run_sim = clumsybot_init.run_sim:main'
+        ],
+    },
+)
