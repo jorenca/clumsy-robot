9f6f20e7d37a7baf6665ec73880f99394c678bf7
jorenca <mailnamefrustration@gmail.com>
2021-09-26 13:59:55 +0300

    Add button to toggle CocoSsd object detection

--- bot-control/src/components/MLAnnotations.js
+++ bot-control/src/components/MLAnnotations.js
@@ -1,47 +1,50 @@
 import React, { useState, useRef, useEffect } from 'react';
 import './MLAnnotations.scss';
 
-const ANNOTATION_INTERVAL_MS = 100;
+const ANNOTATION_INTERVAL_MS = 120;
 let model = null;
 
 const getCameraFeedElement = () => document.getElementById('camera-feed');
 
-export default function MLAnnotations({ sharedTelemetry }) {
+
+
+function CocoSsdAnnotations({ enabled }) {
   const [predictions, setPredictions] = useState([]);
   const detectionTimeoutRef = useRef();
 
-  const predict = async () => {
-    if (!model) {
-      model = await window.cocoSsd.load();
-    }
-    const newPredictions = await model.detect(getCameraFeedElement());
-    setPredictions(newPredictions);
-
-    detectionTimeoutRef.current = setTimeout(predict, ANNOTATION_INTERVAL_MS);
-  };
-
   useEffect(() => {
+    const predict = async () => {
+      if (enabled) {
+        if (!model) {
+          model = await window.cocoSsd.load();
+        }
+        setPredictions(await model.detect(getCameraFeedElement()));
+      }
+
+      detectionTimeoutRef.current = setTimeout(predict, ANNOTATION_INTERVAL_MS);
+    };
+
     detectionTimeoutRef.current = setTimeout(predict, ANNOTATION_INTERVAL_MS);
     return () => clearTimeout(detectionTimeoutRef.current);
-  }, []); // Make sure the effect runs only once
+  }, [enabled]);
 
   const cameraFeedElement = getCameraFeedElement();
-  if (!cameraFeedElement || !cameraFeedElement.complete) {
+  if (!enabled || !cameraFeedElement || !cameraFeedElement.complete) {
     return null;
   }
 
-  const createAnnotationElements = prediction => {
+  const createAnnotationElements = (prediction, index) => {
     const { bbox, score } = prediction;
 
     const annotationElementStyle = {
       left: bbox[0],
-      top: bbox[1],
+      top: Math.max(bbox[1], 0),
       width: bbox[2],
       height: Math.min(bbox[3], cameraFeedElement.height - bbox[1] - 5) // make sure annotation doesn't go outside image
     };
 
     return (
-      <div className="object-annotation" style={annotationElementStyle}>
+      <div className="object-annotation" style={annotationElementStyle} key={index}>
         <p className="annotation-title">
           {prediction.class} ({Math.round(parseFloat(score) * 100)}% confidence)
         </p>
@@ -49,13 +52,24 @@ export default function MLAnnotations({ sharedTelemetry }) {
     );
   };
 
+  return predictions
+            .filter(p => p.score > 0.60)
+            .map(createAnnotationElements);
+}
+
+export default function MLAnnotations() {
+  const [cocoModelEnabled, setCocoModelEnabled] = useState(false);
+
+
   return (
     <div className="ml-annotation-container">
-      {
-        predictions
-            .filter(p => p.score > 0.60)
-            .map(createAnnotationElements)
-      }
+      <div className="ml-activate-buttons">
+        <button onClick={() => setCocoModelEnabled(!cocoModelEnabled)}>
+          {cocoModelEnabled ? 'Disable' : 'Enable'} CocoSsd detection
+        </button>
+        <button>Enable person detection</button>
+      </div>
+      <CocoSsdAnnotations enabled={cocoModelEnabled} />
     </div>
   );
 }
